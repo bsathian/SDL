fc73239d820d237a9a36ef36a749b40e8fbca662
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-08-04 17:24:32 -0700

    Event file

---
+++ Event.cu
@@ -0,0 +1,187 @@
+# include "Event.cuh"
+
+Event::Event()
+{
+    //reset the arrays
+    for(int i = 0; i<6; i++)
+    {
+        n_hits_by_layer_barrel_[i] = 0;
+        n_minidoublets_by_layer_barrel_[i] = 0;
+        if(i<5)
+        {
+            n_hits_by_layer_endcap_[i] = 0;
+            n_minidoublets_by_layer_endcap_[i] = 0;
+        }
+    }
+}
+
+Event::~Event()
+{
+}
+
+void initModules()
+{
+    if(modulesInGPU.detids == nullptr) //check for nullptr and create memory
+    {
+        loadModulesFromFile(modulesInGPU,nModules); //nModules gets filled here
+    }
+    resetObjectRanges(modulesInGPU,nModules);
+}
+
+void Event::resetObjectsInModule()
+{
+    resetObjectRanges(modulesInGPU,nModules);
+}
+
+void Event::addHitToEvent(float x, float y, float z, unsigned int detId)
+{
+    const int HIT_MAX = 1000000;
+    const int HIT_2S_MAX = 100000;
+
+    if(hitsInGPU.xs == nullptr)
+    {
+        createHitsInUnifiedMemory(hitsInGPU,HIT_MAX,HIT_2S_MAX);
+    }
+    //calls the addHitToMemory function
+    addHitToMemory(hitsInGPU, modulesInGPU, x, y, z, detId);
+
+    unsigned int moduleLayer = modulesInGPU.layers[detIdToIndex[detId]];
+    unsigned int subdet = modulesInGPU.subdets[detIdToIndex[detId]];
+
+    if(subdet == Barrel)
+    {
+        n_hits_by_layer_barrel_[moduleLayer]++;
+    }
+    else
+    {
+        n_hits_by_layer_endcap_[moduleLayer]++;
+    }
+
+}
+
+void Event::addMiniDoubletsToEvent()
+{
+    for(unsigned int i = 0; i<nModules; i++)
+    {
+        modulesInGPU.mdRanges[i * 2] = i * N_MAX_MD_PER_MODULES;
+        modulesInGPU.mdRanges[i * 2 + 1] = (i * N_MAX_MD_PER_MODULES) + nMDs[i];
+     
+        if(modulesInGPU.subdets[i] == Barrel)
+        {
+            n_minidoublets_by_layer_barrel_[modulesInGPU.layers[i] -1] ++;
+        }
+        else
+        {
+            n_minidoublets_by_layer_endcap_[modulesInGPU.layers[i] - 1] ++;
+        }
+    }
+}
+
+void Event::createMiniDoublets()
+{
+    unsigned int nLowerModules = *modulesInGPU.nLowerModules;
+    dim3 nThreads(1,16,16);
+    dim3 nBlocks((nLowerModules % nThreads.x == 0 ? nModules/nThreads.x : nModules/nThreads.x + 1),(N_N_MAX_HITS_PER_MODULE_PER_MODULE % nThreads.y == 0 ? N_MAX_HITS_PER_MODULE/nThreads.y : N_MAX_HITS_PER_MODULE/nThreads.y + 1), (N_MAX_HITS_PER_MODULE % nThreads.z == 0 ? N_MAX_HITS_PER_MODULE/nThreads.z : N_MAX_HITS_PER_MODULE/nThreads.z + 1));
+    std::cout<<nBlocks.x<<" "<<nBlocks.y<<" "<<nBlocks.z<<" "<<std::endl;
+    
+    createMiniDoubletsInGPU<<<nBlocks,nThreads>>>(modulesInGPU,hitsInGPU,mdsInGPU);
+
+}
+
+
+__global__ void createMiniDoubletsInGPU(struct modules& modulesInGPU, struct hits& hitsInGPU, struct miniDoublets& mdsInGPU)
+{
+    int lowerModuleArrayIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(lowerModuleIdx > (*modulesInGPU.nLowerModules)) return; //extra precaution
+
+    int lowerModuleIdx = modulesInGPU.lowerModuleIndices[lowerModuleArrayIdx];
+    int upperModuleIdx = modulesInGPU.partnerModuleIndex(lowerModuleIdx);
+    int lowerHitIdx = blockIdx.y * blockDim.y + threadIdx.y;
+    int upperHitIdx = blockIdx.z * blockDim.z + threadIdx.z;
+
+    unsigned int nLowerHits = modulesInGPU.hitRanges[lowerModuleIdx * 2 + 1] - modulesInGPU.hitRanges[lowerModuleIdx * 2] + 1;
+    unsigned int nUpperHits = modulesInGPU.hitRanges[upperModuleIdx * 2 + 1] - modulesInGPU.hitRanges[upperModuleIdx * 2] + 1;
+    //consider assigining a dummy computation function for these
+    if(lowerHitIdx > nLowerHits) return;
+    if(upperHitIdx > nUpperHits) return;
+
+    unsigned int lowerHitArrayIndex = modulesInGPU.hitRanges[lowerModuleIdx * 2] + lowerHitIdx;
+    unsigned int upperHitArrayIndex = modulesInGPU.hitRanges[upperModuleIdx * 2] + upperHitIdx;
+
+    float dz, dphi, dphichange, shiftedX, shiftedY, shiftedZ, noShiftedDz, noShiftedDphi, noShiftedDphiChange;
+    bool success = runMiniDoubletDefaultAlgo(modulesInGPU, hitsInGPU, lowerModuleIdx, lowerHitArrayIndex, upperHitArrayIndex, dz, dphi, dphichange, shiftedX, shfitedY, shiftedZ, noShfitedDz, noShiftedDphi, noShiftedDphiChange);
+    if(success)
+    {
+        unsigned int mdModuleIdx = atomicAdd(&mdsInGPU.nMDs[lowerModuleIdx],1);
+        unsigned int mdIdx = lowerModuleIdx * N_MAX_MD_PER_MODULES + mdModuleIdx;
+
+        addMDToMemory(mdsInGPU,hitsInGPU, modulesInGPU, lowerHitArrayIndex, upperHitArrayIndex, lowerModuleIdx, dz, dphi, dphichange, shiftedX, shiftedY, shiftedZ, noShiftedDz, noShiftedDphi, noShiftedDphiChange, mdIdx);
+    }
+}
+
+unsigned int Event::getNumberOfHits()
+{
+    unsigned int hits = 0;
+    for(auto &it:n_hits_by_layer_barrel_)
+    {
+        hits += it;
+    }
+    void(auto &it:n_hist_by_layer_endcap_)
+    {
+        hits += it;
+    }
+
+    return hits;
+}
+
+unsigned int Event::getNumberOfHitsByLayer(unsigned int layer)
+{
+    if(layer == 6)
+        return n_hits_by_layer_barrel_[layer];
+    else
+        return n_hits_by_layer_barrel_[layer] + n_hits_by_layer_endcap_[layer];
+}
+
+unsigned int Event::getNumberOfHitsBylayerBarrel(unsigned int layer)
+{
+    return n_hits_by_layer_barrel_[layer];
+}
+
+unsigned int Event::getNumberOfHitsByLayerEndcap(unsigned int layer)
+{
+    return n_hits_by_layer_endcap_[layer];
+}
+
+unsigned int Event::getNumberOfMiniDoublets()
+{
+     unsigned int miniDoublets = 0;
+    for(auto &it:n_hits_by_layer_barrel_)
+    {
+        hits += it;
+    }
+    void(auto &it:n_hist_by_layer_endcap_)
+    {
+        hits += it;
+    }
+
+    return hits;
+   
+}
+
+unsigned int Event::getNumberOfMiniDoubletsByLayer(unsigned int layer)
+{
+     if(layer == 6)
+        return n_minidoublets_by_layer_barrel_[layer];
+    else
+        return n_minidoublets_by_layer_barrel_[layer] + n_minidoublets_by_layer_endcap_[layer];   
+}
+
+unsigned int Event::getNumberOfMiniDoubletsByLayerBarrel(unsigned int layer)
+{
+    return n_minidoublets_by_layer_barrel_[layer];
+}
+
+unsigned int Event::getNumberOfMiniDoubletsByLayerEndcap(unsigned int layer)
+{
+    return n_minidoublets_by_layer_endcap_[layer];
+}
---
+++ Event.cuh
@@ -0,0 +1,68 @@
+#ifndef Event_h
+#define Event_h
+
+#include <vector>
+#include <list>
+#include <map>
+#include <stdlib.h>
+#include <stdexcept>
+#include <iostream>
+#include <cmath>
+#include <cuda_runtime.h>
+#include <omp.h>
+
+#include "Module.cuh"
+#include "Hit.cuh"
+#include "MiniDoublet.cuh"
+#include "GeometryUtil.cuh"
+#include "cuda_profiler_api.h"
+
+class Event
+{
+private:
+    std::array<unsigned int, 6> n_hits_by_layer_barrel_;
+    std::array<unsigned int, 5> n_hits_by_layer_endcap_;
+    std::array<unsigned int, 6> n_minidoublets_by_layer_barrel_;
+    std::array<unsigned int, 5> n_minidoublets_by_layer_endcap_;
+    void incrementNumberOfMiniDoublets();
+
+    //CUDA stuff
+    struct hits hitsInGPU;
+    struct miniDoublets mdsInGPU;
+    int nHits;
+    int nMiniDoublets;
+
+
+public:
+    Event();
+    ~Event();
+
+    void addHitToEvent(float x, float y, float z, unsigned int detId); //call the appropriate hit function, then increment the counter here
+    void addMiniDoubletsToEvent();
+    void resetObjectsInModule();
+    void createMiniDoublets();
+
+    unsigned int getNumberOfHits();
+    unsigned int getNumberOfHitsByLayer(unsigned int layer);
+    unsigned int getNumberOfHitsByLayerBarrel(unsigned int layer);
+    unsigned int getNumberOfHitsByLayerEndcap(unsigned int layer);
+
+    unsigned int getNumberOfMiniDoublets();
+    unsigned int getNumberOfMiniDoubletsByLayer(unsigned int layer);
+    unsigned int getNumberOfMiniDoubletsByLayerBarrel(unsigned int layer);
+    unsigned int getNumberOfMiniDoubletsByLayerEndcap(unsigned int layer);
+};
+
+//global stuff
+int N_MAX_MD_PER_MODULES = 100;
+int N_MAX_HITS_PER_MODULE = 100;
+
+struct modules modulesInGPU;
+int nModules;
+void initModules(); //read from file and init
+
+__global__ void createMiniDoubletsInGPU(struct hits& hitsInGPU, struct modules& modulesInGPU, struct miniDoublets& mdsInGPU);
+
+
+
+#endif
